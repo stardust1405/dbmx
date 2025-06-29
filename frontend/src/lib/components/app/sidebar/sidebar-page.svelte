c591b46e94b549a2c87d40d0cbf8035fa6baf83b
Karan Singhasane <ksinghasane14@gmail.com>
2025-06-29 16:23:12 +0530

    remove unused file

--- frontend/src/lib/components/app/sidebar/sidebar-page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import SettingsDialog from '$lib/components/app/sidebar/settings-dialog.svelte';
-</script>
-
-<div class="flex h-svh items-center justify-center">
-	<SettingsDialog />
-</div>
