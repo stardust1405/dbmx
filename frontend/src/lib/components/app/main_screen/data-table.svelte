429f2f86704a5ac1406df0339ed9f083dc513c9a
Karan Singhasane <ksinghasane14@gmail.com>
2025-07-12 01:09:48 +0530

    remove unwanted code and update query locading

--- frontend/src/lib/components/app/main_screen/data-table.svelte
+++ frontend/src/lib/components/app/main_screen/data-table.svelte
@@ -105,6 +105,8 @@
 			}
 		}
 	});
+
+	queryLoading = false;
 </script>
 
 <div class="h-full overflow-auto">
@@ -247,26 +249,6 @@
 		</div>
 	</div>
 </div>
-
-{#snippet DataTableTarget({ row }: { row: RowData })}
-	<form
-		onsubmit={(e) => {
-			e.preventDefault();
-			// toast.promise(new Promise((resolve) => setTimeout(resolve, 1000)), {
-			// 	loading: `Saving ${row.id}`,
-			// 	success: 'Done',
-			// 	error: 'Error'
-			// });
-		}}
-	>
-		<Label for="target" class="sr-only">Target</Label>
-		<Input
-			class="hover:bg-input/30 focus-visible:bg-background dark:hover:bg-input/30 dark:focus-visible:bg-input/30 h-8 w-16 border-transparent bg-transparent text-right shadow-none focus-visible:border dark:bg-transparent"
-			value="abc"
-			id="target"
-		/>
-	</form>
-{/snippet}
 
 <style>
 	:global(table th),
